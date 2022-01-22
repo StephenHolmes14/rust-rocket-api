4fc7ce487087701ee9ee533781198d8e1fe55141
StephenHolmes14 <Stephenholmes1995@googlemail.com>
2022-01-22 20:28:23 +0000

    Basic endpoint to parse integers from String param

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+use rocket::serde::json::{json, Value};
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
+}
+
+// Try make an api which takes a String integer (i32) and returns as an i32. If not i32 return -1
+#[get("/parse_integer/<i32_string>")]
+fn parse_integer(i32_string : String) -> Value {
+    let num_result = i32_string.parse::<i32>();
+
+    if let Err(_num_result) = num_result {
+        return json!(-1);
+    }
+
+    return json!(num_result.unwrap());
 }
 
 #[get("/<name>/<age>")]
@@ -12,6 +25,7 @@ fn hello_name_age(name: String, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 
-fn main() {
-  rocket::ignite().mount("/", routes![index, hello_name_age]).launch();
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, hello_name_age, parse_integer])
 }
